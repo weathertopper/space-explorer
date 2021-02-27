b8e8f95294c08b600b879039dbfd238bb36d3a3e
weathertopper <nathanweatherly94@gmail.com>
2021-02-26 22:54:47 -0500

    init commit

---
+++ space-explorer/Assets/AstroBody.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class AstroBody : MonoBehaviour
+{
+
+    public GameObject body;
+    public float mass;
+
+    public AstroBody(string name, Vector3 startPos, float diameter, float density)
+    {
+        this.body = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        this.body.name = name;
+        this.body.transform.position = startPos;
+        this.body.transform.localScale = new Vector3(diameter, diameter, diameter);
+        float radius = (float) (diameter / 2);
+        float volume = (float) (( 4 * Math.PI * radius * radius * radius) / 3 );
+        this.mass = density * volume;
+    }
+
+    public float getMass()
+    {
+        return this.mass;
+    }
+
+}
---
+++ space-explorer/Assets/BlastOff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BlastOff : MonoBehaviour
+{
+
+    public List<AstroBody> solarSystem;
+    // public GameObject main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        solarSystem = new List<AstroBody>();
+        solarSystem.Add(new AstroBody("moon", new Vector3(1.0f, 1.0f, 1.0f), 1, 1));
+        solarSystem.Add(new AstroBody("other moon", new Vector3(-3.0f, -3.0f, -3.0f), 2, 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print("Hello Update!");
+    }
+}
